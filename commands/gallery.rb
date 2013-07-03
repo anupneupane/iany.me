e4eff271e3f4884bf556586a9de5472437be2482
Ian Yang <me@iany.me>
2013-07-03 10:14:40 +0800

    Fix gallery sync location

--- commands/gallery.rb
+++ commands/gallery.rb
@@ -3,8 +3,8 @@ summary 'Push gallery to server or pull from server'
 
 run do |opts, args, cmd|
   if args.first == 'pull'
-    system 'rsync -rv --size-only --progress iany.me:/mnt/iany.me/shared/content/gallery content/'
+    system 'rsync -rv --size-only --progress iany.me:/mnt/iany.me/shared/gallery content/'
   else
-    system 'rsync -rv --size-only --progress content/gallery iany.me:/mnt/iany.me/shared/content/'
+    system 'rsync -rv --size-only --progress content/gallery iany.me:/mnt/iany.me/shared/'
   end
 end
